c93038d322c7a55d3f977720a8a462da80b25a87
Harsh Patel <harsh.patel4@somaiya.edu>
2018-12-09 14:40:02 +0530

    Initial commit

---
+++ image_gallery.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Image gallery</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="image_gallery.css">
+</head>
+<body>
+	<h1>
+</body>
+</html>
